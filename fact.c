4460148eaf6514a507932df75ff316826ed4eaf4
karthikreddi2016 <vkarthikreddy005@gmail.com>
2025-02-05 09:21:34 +0530

    Create fact.c

---
+++ fact.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+// Function to calculate factorial
+long long factorial(int n) {
+    if (n < 0) {
+        return -1; // Factorial is not defined for negative numbers
+    } else if (n == 0) {
+        return 1;
+    } else {
+        long long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
+
+int main() {
+    int num;
+
+    printf("Enter a non-negative integer: ");
+    scanf("%d", &num);
+
+    long long fact = factorial(num);
+
+    if (fact == -1) {
+        printf("Factorial is not defined for negative numbers.\n");
+    } else {
+        printf("The factorial of %d is %lld\n", num, fact);
+    }
+
+    return 0;
+}
